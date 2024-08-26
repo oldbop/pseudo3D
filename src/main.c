fc89b73f17a4b050f97bb643e0a3133172f2f1b9
oldbop <oliverdavybowker@gmail.com>
2024-08-26 11:40:54 +0100

    Change struct initializers

--- src/main.c
+++ src/main.c
@@ -9,12 +9,12 @@
 #ifdef _WIN32
 #include <windows.h>
 
-#define SLEEP(ms) Sleep(ms)
+#define SLEEP_MS(ms) Sleep(ms)
 
 #else
 #include <sys/select.h>
 
-#define SLEEP(ms)                                         \
+#define SLEEP_MS(ms)                                      \
 do {                                                      \
   struct timeval delay = { 0, ms * 1.0E+3 };              \
                                                           \
@@ -41,26 +41,58 @@ do {                                                      \
 
 #define COLOR(r, g, b, a) (r << 24) | (g << 16) | (b << 8)  | (a << 0)
 
-#define TITLE      "pseudo3D"
-#define SCR_WIDTH  960
-#define SCR_HEIGHT 720
-#define DSCALE     4
-#define FPS_CAP    2000
-#define PI         3.141592653589793f
+#define TITLE   "pseudo3D"
+#define MAP_W   8
+#define MAP_H   8
+#define SCR_W   960
+#define SCR_H   720
+#define DSCALE  4
+#define FPS_CAP 2000
+#define PI      3.141592653589793f
 
 typedef float vec2f[2];
+typedef float vec3f[3];
+typedef int32_t vec2i[2];
+typedef int32_t vec3i[3];
 
-struct Renderer {
+typedef uint8_t Map[MAP_W * MAP_H];
+
+typedef struct {
   int32_t width, height, cx, cy, npixels;
   uint32_t *pixels, ftex, fbo;
-  double lastf, lastt, minspf;
+  double dlastf, dlastt, minspf;
   char title[32];
   GLFWwindow *win;
-} rdr;
+} Renderer;
 
-struct GameState {
+typedef struct {
   vec2f pos, dir;
-} sta;
+  double time;
+  Map map;
+} GameState;
+
+Renderer rdr = {
+  .width   = SCR_W / DSCALE,
+  .height  = SCR_H / DSCALE,
+  .cx      = SCR_W / (DSCALE * 2),
+  .cy      = SCR_H / (DSCALE * 2),
+  .npixels = (SCR_W / DSCALE) * (SCR_H / DSCALE),
+  .minspf  = 1.0 / FPS_CAP
+};
+
+GameState sta = {
+  .pos = { MAP_W / 2.0f, MAP_H / 2.0f }, .dir = { 0.0f, 1.0f },
+  .map = {
+    1, 1, 1, 1, 1, 1, 1, 1,
+    1, 0, 0, 0, 0, 1, 0, 1,
+    1, 0, 0, 0, 0, 1, 0, 1,
+    1, 0, 0, 0, 0, 1, 0, 1,
+    1, 0, 0, 0, 0, 1, 0, 1,
+    1, 0, 0, 0, 1, 1, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 1,
+    1, 1, 1, 1, 1, 1, 1, 1
+  }
+};
 
 static inline void clear_screen(uint32_t color) {
   for (int32_t i = 0; i < rdr.npixels; ++i)
@@ -95,7 +127,7 @@ static inline void render(void) {
                   rdr.pixels);
 
   glBlitFramebuffer(0, 0, rdr.width, rdr.height,
-                    0, 0, SCR_WIDTH, SCR_HEIGHT,
+                    0, 0, SCR_W, SCR_H,
                     GL_COLOR_BUFFER_BIT,
                     GL_NEAREST);
 
@@ -114,21 +146,12 @@ int main(int argc, char **argv) {
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_RESIZABLE, 0);
 
-  rdr.win = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, TITLE, NULL, NULL);
+  rdr.win = glfwCreateWindow(SCR_W, SCR_H, TITLE, NULL, NULL);
 
   if (!rdr.win) {
     printf("GLFW: failed to create window\n");
     return -1;
   }
-
-  rdr.width   = SCR_WIDTH / DSCALE;
-  rdr.height  = SCR_HEIGHT / DSCALE;
-  rdr.cx      = rdr.width / 2;
-  rdr.cy      = rdr.height / 2;
-  rdr.npixels = rdr.width * rdr.height;
-  rdr.lastf   = 0.0;
-  rdr.lastt   = 0.0;
-  rdr.minspf  = 1.0 / FPS_CAP;
 
   rdr.pixels = malloc(rdr.npixels * sizeof(uint32_t));
 
@@ -142,7 +165,7 @@ int main(int argc, char **argv) {
 
   LOAD_GL();
 
-  glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+  glViewport(0, 0, SCR_W, SCR_H);
 
   glGenTextures(1, &rdr.ftex);
   glBindTexture(GL_TEXTURE_2D, rdr.ftex);
@@ -163,18 +186,21 @@ int main(int argc, char **argv) {
                          GL_TEXTURE_2D,
                          rdr.ftex, 0);
 
+  double lastf = 0.0, lastt = 0.0;
+
   while (!glfwWindowShouldClose(rdr.win)) {
+
+    sta.time = glfwGetTime();
+
+    rdr.dlastf = sta.time - lastf;
+    rdr.dlastt = sta.time - lastt;
 
     process_input();
 
-    double time   = glfwGetTime();
-    double dlastf = time - rdr.lastf;
-    double dlastt = time - rdr.lastt;
-
-    if (dlastt >= 0.5) {
-      snprintf(rdr.title, 32, "%s [FPS: %.2f]", TITLE, 1.0 / dlastf);
+    if (rdr.dlastt >= 0.5) {
+      snprintf(rdr.title, 32, "%s [FPS: %.2f]", TITLE, 1.0 / rdr.dlastf);
       glfwSetWindowTitle(rdr.win, rdr.title);
-      rdr.lastt = time;
+      lastt = sta.time;
     }
 
     clear_screen(COLOR(0x1a, 0x1a, 0x1a, 0xff));
@@ -183,9 +209,10 @@ int main(int argc, char **argv) {
       draw_vert_line(COLOR(0xff, 0xff, 0xff, 0xff), i, 0.5f);
 
     render();
-    rdr.lastf = time;
+    lastf = sta.time;
 
-    SLEEP((time + rdr.minspf - glfwGetTime()) * 1.0E+3);
+    SLEEP_MS((sta.time + rdr.minspf - glfwGetTime()) * 1.0E+3);
+
     glfwPollEvents();
   }
 
